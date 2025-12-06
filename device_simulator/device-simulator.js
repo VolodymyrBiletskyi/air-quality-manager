c48127cdb8cacd42a837aafbc6198f38b9fdd2e7
Outlines1 <169167864+Outlines1@users.noreply.github.com>
2025-12-06 08:55:08 +0100

    Device sim (#1)
    * Added Air Quality Device Simulator (test version )
    
    * Added backend for testing device-simulator(local only)

---
+++ device_simulator/device-simulator.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+const BACKEND_URL = "http://localhost:3000/api/readings";//process.env.BACKEND_URL || 'http://localhost:3000/api/telemetry';
+const DEVICE_ID = "sensor-001";/*process.env.DEVICE_ID || "js-sim-01";*/
+const INTERVAL = 5000;//parseInt(process.env.INTERVAL || "5000");
+
+function generateReading(){
+    return {
+        device_id: DEVICE_ID,
+        timestamp: new Date().toISOString(),
+        pm25: Number((Math.random() * 80).toFixed(2)),
+        pm10: Number((Math.random() * 120).toFixed(2)),
+        co2: Number((400 + Math.random() * 1000).toFixed(1)),
+        temp: Number((15 + Math.random() * 15).toFixed(2)),
+        humidity: Number((20 + Math.random() * 60).toFixed(2))
+        };
+}
+
+async function main() {
+    console.log("Device Simulator started; Sending telemetry to:", BACKEND_URL)
+
+    while(true){
+        const payload = generateReading();
+        console.log("generated", payload);
+        try{
+            const res = await axios.post(BACKEND_URL, payload);
+            console.log("Sent: ", payload, "->", res.status);
+        }
+        catch(err){
+            console.error("Send failed", err.message);
+        }
+        await new Promise(resolve => setTimeout(resolve, INTERVAL));
+    }
+}
+
+main();
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 8814;
+const port = 3000 //process.env.PORT || 8814;
+app.use(cors());
+app.use(express.json());
+
+let memoryStorage = [];
 
 app.get('/', (req, res) => {
     res.send("<h1>Sosal?</h1>");
 })
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+app.post("/api/readings", (req, res) => {
+    const reading = req.body;
+    if(!reading.device_id){
+        return res.status(400).json({error: "device_id is required"});
+    }
+
+    memoryStorage.push(reading);
+    console.log("Received reading:", reading);
+    res.status(201).json({message: "Reading stored"});
+});
+
+app.get("/api/readings", (req, res) => {
+   res.json(memoryStorage);
+});
+
+app.listen(port, () => console.log("Backend running on http://localhost:" + port));
