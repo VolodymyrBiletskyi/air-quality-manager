be77af35d5bea353a7eeaf86c308276ad3c27593
Volodymyr Biletskyi <vladimirbilez@gmail.com>
2025-12-10 17:54:56 +0100

    measurement controller

--- Extensions/measurements.js
+++ Extensions/measurements.js
@@ -1,48 +1,45 @@
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
 
-export const addMeasurement = async (req, res) => {
-    try {
-        const {
-            device_id,
-            timestamp,
-            pm25,
-            pm10,
-            co2,
-            temp,
-            humidity,
-            aqi,
-            category,
-            health_message
-        } = req.body;
+export async function saveMeasurementsFromPayload(payload) {
+    const {
+        device_id,
+        timestamp,
+        pm25,
+        pm10,
+        co2,
+        temp,
+        humidity,
+        aqi,
+        category,
+        health_message
+    } = payload;
 
-        const device = await prisma.device.findUnique({
-            where: { id: device_id },
-        });
+    const device = await prisma.device.findUnique({
+        where: {id: device_id},
+    });
 
-        if (!device)
-            return res.status(404).json({ error: "Device not found" });
-
-        const saved = await prisma.measurement.create({
-            data: {
-                deviceId: device_id,
-                timestamp: new Date(timestamp),
-                pm2_5: pm25,
-                pm10: pm10,
-                co2: co2,
-                temperature: temp,
-                humidity: humidity,
-                aqi: aqi ?? null,
-                category: category ?? null,
-                healthMessage: health_message ?? null
-            },
-        });
-
-        return res.status(201).json(saved);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
+    if (!device) {
+        throw new Error("Device not found.");
     }
-};
+
+
+    const saved = await prisma.measurement.create({
+        data: {
+            deviceId: device_id,
+            timestamp: new Date(timestamp),
+            pm2_5: pm25,
+            pm10: pm10,
+            co2: co2,
+            temperature: temp,
+            humidity: humidity,
+            aqi: aqi ?? null,
+            category: category ?? null,
+            healthMessage: health_message ?? null
+        },
+    });
+
+    return saved;
+}
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import userRouter from './Controllers/UserController.js';
-import { addMeasurement } from "./Extensions/measurements.js";
 import deviceRouter from './Controllers/DeviceController.js';
+import measurementRouter from "./Controllers/MeasurementController.js";
 
 
 const app = express();
@@ -13,15 +13,13 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use('/api/devices', deviceRouter);
 app.use('/api/users', userRouter);
+app.use("/api/measurements", measurementRouter);
 
 let memoryStorage = [];
 
 app.get('/', (req, res) => {
     res.send("<h1>Sosal?</h1>");
 })
-
-app.post("/api/measurements", addMeasurement);
-
 
 app.get("/api/readings", (req, res) => {
    res.json(memoryStorage);
